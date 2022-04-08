9d1aceb87bd25aae872231429ca958d1a6b9e7ad
RolandYambao <rolandyambao@gmail.com>
2022-04-08 02:25:25 -0700

    Updated

---
+++ src/components/Repair.js
@@ -0,0 +1,20 @@
+import './Repair.css';
+import React, { Component } from 'react';
+
+class Repair extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Testing</p>
+            </div>
+        )
+    }
+
+}
+
+export default Repair;
