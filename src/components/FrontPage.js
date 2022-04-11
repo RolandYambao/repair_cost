72ae63e3570d35b4313e9da7e70871e379fd1072
RolandYambao <rolandyambao@gmail.com>
2022-04-11 03:35:37 -0700

    Updated

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -19,7 +19,7 @@ const FrontPage = () => {
                 <div className="level">
                     <div className="level-left">
                         <div className="level-item">
-                            <h2 className="subtitle">Items availabe on Mercer:</h2>
+                            <h2 className="subtitle">Services Available in RepairCo LLC.</h2>
                         </div>
                     </div>
                 </div>
@@ -28,52 +28,45 @@ const FrontPage = () => {
                         <a href="/viewitems">
                             <article>
                                 <figure className="image is-5by3">
-                                    <img src="https://makinglemonadeblog.com/wp-content/uploads/2015/06/popular-yard-sale-garage-sale-outdoor-furniture.jpg" />
+                                    <img src="https://www.piercecountywa.gov/ImageRepository/Document?documentID=74192" alt="Home Repair" />
                                 </figure>
-                                <h2 className="subtitle">Outdoor Furniture</h2>
+                                <h2 className="subtitle">Home Repair</h2>
                             </article>
                         </a>
                     </div>
 
                     <div className="column is-3">
-                        <a href="/viewitems">
-                            <article>
-                                <figure className="image is-5by3">
-                                    <img
-                                        src="https://makinglemonadeblog.com/wp-content/uploads/2015/06/popular-yard-sale-garage-sale-lawn-equipment.jpg"
-                                        alt="Yard Sale"
-                                    />
-                                </figure>
-                                <h2 className="subtitle">Lawn Equipment</h2>
-                            </article>
-                        </a>
+                        <article>
+                            <figure className="image is-5by3">
+                                <img
+                                    src="https://athome-solutions.com/wp-content/uploads/2017/07/Appliance-Repairs-1024x683.jpg"
+                                    alt="Appliance Repair"
+                                />
+                            </figure>
+                            <h2 className="subtitle">Appliance Repair</h2>
+                        </article>
                     </div>
                     <div className="column is-3">
-                        <a href="/viewitems">
-                            <article>
-                                <figure className="image is-5by3">
-                                    <img
-                                        src="https://makinglemonadeblog.com/wp-content/uploads/2015/06/best-items-to-sell-yard-garage-sales-kids-toys.jpg"
-                                        alt="Garage Sale"
-                                    />
-                                </figure>
-                                <h2 className="subtitle">Children's Toys</h2>
-                            </article>
-                        </a>
+                        <article>
+                            <figure className="image is-5by3">
+                                <img
+                                    src="https://hubautoparts.com/wp-content/uploads/2017/08/can-routine-vehicle-maintenance-improve-gas-mileage.jpeg"
+                                    alt="Car Repair"
+                                />
+                            </figure>
+                            <h2 className="subtitle">Car Repair</h2>
+                        </article>
                     </div>
                     <div className="column is-3">
-                        <a href="/viewitems">
-                            <article>
-                                <figure className="image is-5by3">
-                                    <img
-                                        src="https://makinglemonadeblog.com/wp-content/uploads/2015/06/popular-yard-sale-garage-sale-costume-jewelry.jpg"
-                                        alt="Yard Sale Items"
-                                    />
-                                </figure>
-                                <h2 className="subtitle">Jewelry</h2>
-                                <br />
-                            </article>
-                        </a>
+                        <article>
+                            <figure className="image is-5by3">
+                                <img
+                                    src="https://i.imgur.com/xQfwpjq.jpeg"
+                                    alt="Repair Logo"
+                                />
+                            </figure>
+                            <h2 className="subtitle">and Many More....</h2>
+                        </article>
                     </div>
                 </div>
             </section>
