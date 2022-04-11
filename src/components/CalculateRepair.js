0c40d79b617df34e2844d280e915d93c297fa546
RolandYambao <rolandyambao@gmail.com>
2022-04-10 21:54:52 -0700

    Updated

--- src/components/CalculateRepair.js
+++ src/components/CalculateRepair.js
@@ -106,13 +106,13 @@ class CalculateRepair extends Component {
                     <div className="container">
                         <div className="navbar-brand">
                             <img id="logo" src="https://i.imgur.com/xQfwpjq.jpeg" alt="repairLogo" />
+                            <strong><a id="companyName">RepairCo LLC.</a></strong>
                             <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                                 <span aria-hidden="true"></span>
                                 <span aria-hidden="true"></span>
                                 <span aria-hidden="true"></span>
                             </a>
                         </div>
-
                         <div id="navbarBasicExample" className="navbar-menu">
                             <div className="navbar-end">
                                 <a className="navbar-item"> Home </a>
